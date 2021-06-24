73089ee57807e6296c48670f2c321b70bbd4933b
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-06-24 13:29:24 -0400

    basic inquirer prompts set up

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 // packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const getInput = () => {
+    return inquirer.prompt(
+        [
+            {
+                type: 'input',
+                message: 'Enter first name.',
+                name: 'name',
+            },
+            {
+                type: 'list',
+                message: 'Choose position.',
+                name: 'position',
+                choices: [
+                    'manager',
+                    'engineer',
+                    'intern',
+                ]
+            },
+            {
+                type: 'input',
+                message: 'Enter ID number.',
+                name: 'id',
+            },
+            {
+                type: 'input',
+                message: 'Enter email address.',
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: 'Enter github username.',
+                name: 'username',
+            },
+            {
+                type: 'input',
+                message: 'Enter school.',
+                name: 'school',
+            },
+        ]
+        ]
+    )
+}
