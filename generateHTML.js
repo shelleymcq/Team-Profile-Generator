94d18d9b1a0ccb8487dd7272543202d7b5fa3ee1
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-06-24 13:16:55 -0400

    draft html written and styled with bulma

---
+++ generateHTML.js
@@ -0,0 +1,153 @@
+const generateHTML = (answers) =>
+`
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css"></link>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    <title>My Team</title>
+
+</head>
+  <body >
+      <header class="title py-6 has-background-danger has-text-centered">My Team</header>
+
+    <div class="container is-max-desktop p-6">
+        <div class="columns is-mobile is-multiline is-centered">
+
+            <div class="column is-narrow">
+                <div class="box p-0">
+                    <div class="block has-background-info p-2">
+                        <div class="title is-5 has-text-white">
+                            Name
+                        </div>
+                        <div class="subtitle is-6 has-text-white">
+                           Icon + Title
+                        </div>
+                    </div>
+
+                    <div class="block p-3 is-size-7">
+                        <div>
+                            ID#
+                        </div>
+                        <div>
+                            Email
+                        </div>
+                        <div>
+                            Github
+                        </div>
+                    </div>
+                  </div>   
+            </div>
+
+            <div class="column is-narrow">
+                <div class="box p-0">
+                    <div class="block has-background-info p-2">
+                        <div class="title is-5 has-text-white">
+                            Name
+                        </div>
+                        <div class="subtitle is-6 has-text-white">
+                           Icon + Title
+                        </div>
+                    </div>
+
+                    <div class="block p-3 is-size-7">
+                        <div>
+                            ID#
+                        </div>
+                        <div>
+                            Email
+                        </div>
+                        <div>
+                            Github
+                        </div>
+                    </div>
+                  </div>   
+            </div>
+
+            <div class="column is-narrow">
+                <div class="box p-0">
+                    <div class="block has-background-info p-2">
+                        <div class="title is-5 has-text-white">
+                            Name
+                        </div>
+                        <div class="subtitle is-6 has-text-white">
+                           Icon + Title
+                        </div>
+                    </div>
+
+                    <div class="block p-3 is-size-7">
+                        <div>
+                            ID#
+                        </div>
+                        <div>
+                            Email
+                        </div>
+                        <div>
+                            Github
+                        </div>
+                    </div>
+                  </div>   
+            </div>
+
+            <div class="column is-narrow">
+                <div class="box p-0">
+                    <div class="block has-background-info p-2">
+                        <div class="title is-5 has-text-white">
+                            Name
+                        </div>
+                        <div class="subtitle is-6 has-text-white">
+                           Icon + Title
+                        </div>
+                    </div>
+
+                    <div class="block p-3 is-size-7">
+                        <div>
+                            ID#
+                        </div>
+                        <div>
+                            Email
+                        </div>
+                        <div>
+                            Github
+                        </div>
+                    </div>
+                  </div>   
+            </div>
+
+            <div class="column is-narrow">
+                <div class="box p-0">
+                    <div class="block has-background-info p-2">
+                        <div class="title is-5 has-text-white">
+                            Name
+                        </div>
+                        <div class="subtitle is-6 has-text-white">
+                           Icon + Title
+                        </div>
+                    </div>
+
+                    <div class="block p-3 is-size-7">
+                        <div>
+                            ID#
+                        </div>
+                        <div>
+                            Email
+                        </div>
+                        <div>
+                            Github
+                        </div>
+                    </div>
+                  </div>   
+            </div>
+        </div>  
+
+    </div>
+        
+
+
+
+
+  </body>
+</html>
+`
