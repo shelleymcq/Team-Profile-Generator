817a59ec6962dbe4e05e7039a42c1d13b5869140
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-06-29 20:06:17 -0400

    select html template based on employee position

--- src/htmlGenerator.js
+++ src/htmlGenerator.js
@@ -9,7 +9,6 @@ const htmlGenerator = (employeesArray) => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <!-- only keep style.css if everything can't be done in bootstrap -->
         <link rel="stylesheet" href="../dist/style.css">
         <title>Document</title>
     </head>
@@ -18,22 +17,55 @@ const htmlGenerator = (employeesArray) => {
     let bodyHTML = '';
 
     employeesArray.forEach(el => {
-      //  name, id, email
-      bodyHTML += `
-        <div>
-            <h3>Name: ${el.name}<h3>
-            <p>ID: ${el.id}</p>
-            <p>Email: ${el.email}</p>
-        </div>`
+
+      if (el.position === 'manager') {
+        bodyHTML += `
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-4">
+                        <h3>Name: ${el.name}<h3>
+                        <p>ID: ${el.id}</p>
+                        <p>Email: ${el.email}</p>
+                        <p>Position: ${el.position}</p>
+                    </div>
+                </div>
+            </div>`
+      } else if (el.position === 'engineer') {
+        bodyHTML += `
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-4">
+                        <h3>Name: ${el.name}<h3>
+                        <p>ID: ${el.id}</p>
+                        <p>Email: ${el.email}</p>
+                        <p>Position: ${el.position}</p>
+                    </div>
+                </div>
+            </div>`
+      } else if (el.position === 'intern') {
+        bodyHTML += `
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-4">
+                        <h3>Name: ${el.name}<h3>
+                        <p>ID: ${el.id}</p>
+                        <p>Email: ${el.email}</p>
+                        <p>Position: ${el.position}</p>
+                    </div>
+                </div>
+            </div>`
+        }
     })
 
 
-    const closingHTML = `
+
+    const closeHTML = `
     </body>
     </html>`
 
+    // concatenate the strings to form the whole page
 
- return openHTML + bodyHTML + closingHTML;
+ return openHTML + bodyHTML + closeHTML;
 
 }
 
